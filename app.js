285a7fafc674c8919b85d6c8fafb142f2249e9f1
noor <noorhafowbare@gmail.com>
2025-03-11 09:46:00 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Boolean
+let isTrue = true;
+console.log(isTrue);
+
+let num = NaN;
+console.log(num);
+
+let number = 10;
+console.log(number + undefined);
+
+// falsy value
+// false
+// null
+// undefined
+// 0
+// -0
+// NaN
+// '', "", ``, (empty quotes)
